bef6b9fab2450a321ce2b4f8d30a63213f8c0116
odilbek29 <kamoliddinpardaev272@gmail.com>
2025-06-14 08:43:53 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import './App.css'
+
+function App() {
+    return (
+        <div>
+            <header>
+                <div className="container">
+                    <div className="a">
+                        <img src="./Logo.png" alt="sdfv" />
+
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Features</a></li>
+                            <button className='btn'>Register Now </button>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <section className='hero'>
+                <div className="container">
+                    <div className="text">
+                        <h1>Lessons and insights   </h1> <br />
+                        <h2>from 8 years</h2>
+                        <p>Where to grow your business as a photographer: site or social media?</p>
+                        <button className='btn2'>Register</button>
+                    </div>
+
+                    <div className="b">
+                        <img src="2.png" alt="gh" />
+                    </div>
+                </div>
+            </section>
+            <section className='hero2'>
+                <div className="contaimner">
+                    <div className="text2">
+                        <h3>Our Clients</h3> <br />
+                        <p>We have been working with some Fortune 500+ clients</p>
+                    </div>
+                    <div className="ma">
+                        <img src="6.png" alt="dfb" />
+                        <img src="6.png" alt="dfb" />
+                        <img src="6.png" alt="dfb" />
+                        <img src="6.png" alt="dfb" />
+                        <img src="6.png" alt="dfb" />
+                        <img src="6.png" alt="dfb" />
+                        <img src="6.png" alt="dfb" />
+                    </div>
+                </div>
+            </section>
+            <section className='hero3'>
+
+                <h1>Manage your entire community <br /> in a single system</h1>
+                <div className="container">
+              <div className="container">
+
+                <div className="ota">
+                    <div className="card">
+                        <img src="1.png" alt="ghj" />
+                        <h3>Membership <br /> Organisations</h3>
+                        <p>Our membership management software provides full automation of membership renewals and payments</p>
+                    </div>
+                    <div className="card">
+                        <img src="1.png" alt="ghj" />
+                        <h3>Membership <br /> Organisations</h3>
+                        <p>Our membership management software provides full automation of membership renewals and payments</p>
+                    </div>
+                    <div className="card">
+                        <img src="1.png" alt="ghj" />
+                        <h3>Membership <br /> Organisations</h3>
+                        <p>Our membership management software provides full automation of membership renewals and payments</p>
+                    </div>
+                </div>
+ 
+              </div>
+                </div>
+            </section>
+
+            <footer>
+                <div className="container">
+                    <img src="5.png" alt="sdf" />
+                    <div className="text3">
+                        <h1>The unseen of spending three years at Pixelgrade</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur dolores voluptatibus nisi, veniam officiis quod! Commodi rem quaerat beatae incidunt culpa earum! A neque ratione odit ut rerum repudiandae eum. Et odit officiis quaerat, eos odio ipsa ipsam magni quod quibusdam repellendus qui nobis dolor provident, aliquid, quae iusto omnis?</p>
+                        <button className='btn2'>Learn More</button>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default App
